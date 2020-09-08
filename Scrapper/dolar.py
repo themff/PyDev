0382d7870765ab7a3fdd65fb39ec64f85ce891d7
Fk <themff@gmail.com>
2020-09-08 02:29:14 -0300

    seguimos

---
+++ Scrapper/dolar.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://dolarhoy.com/"
+page = requests.get(url)   
+soup = BeautifulSoup(page.content,"html.parser")
+col6 = soup.find_all('div', class_='col-6')
+precio_compra = col6[0].find_all('span')
+print(precio_compra)
--- Scrapper/main.py
+++ Scrapper/main.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.lanacion.com.ar/"
-page = requests.get(url)
+page = requests.get(url)   
 soup = BeautifulSoup(page.content,"html.parser")
-print(soup)
+content_titles = soup.find_all('h2','com-title')
+print(":::::::::::::::TITULARES::::::::::::")
+for tit in content_titles:
+    sub = tit.find_all('a')
+    print(sub[0].attrs.get('title'))    #con attrs.get me traigo el atributo que yo quiera de sub. en este caso me traigo el title.
+    print("https://www.lanacion.com.ar"+sub[0].attrs.get('href'))
+    print("")
